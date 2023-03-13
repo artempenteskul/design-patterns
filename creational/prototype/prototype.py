3953de835d8fc425888e8b67b5b92a50cef7326e
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-03-14 00:09:57 +0200

    refactored prototype pattern example

--- creational/prototype/prototype.py
+++ creational/prototype/prototype.py
@@ -1,45 +1,63 @@
 import copy
 
 
-# Thats just an object with reference for the example
 class SelfReferencingEntity:
-    def __init__(self) -> None:
+    def __init__(self):
         self.parent = None
 
-    def set_parent(self, parent) -> None:
+    def set_parent(self, parent):
         self.parent = parent
 
 
-# Python provides its own interface of Prototype via "copy.copy" and "copy.deepcopy" functions.
-# And any class that wants to implement custom implementations have override '__copy__' and '__deepcopy__' member functions.
 class SomeComponent:
-    def __init__(self, some_int, some_list_of_objects, some_circular_ref) -> None:
+    """
+    Python provides its own interface of Prototype via `copy.copy` and `copy.deepcopy` functions. 
+    And any class that wants to implement custom implementations have to override `__copy__` and `__deepcopy__` member functions.
+    """
+
+    def __init__(self, some_int, some_list_of_objects, some_circular_ref):
         self.some_int = some_int
         self.some_list_of_objects = some_list_of_objects
         self.some_circular_ref = some_circular_ref
 
-    # Creates a shallow copy. This method will be called whenever someone calls 'copy.copy' with this object and the returned value is new shallow copy.
     def __copy__(self):
+        """
+        Create a shallow copy.
+        This method will be called whenever someone calls `copy.copy` with this object and the returned value is returned as the new shallow copy.
+        """
+
+        # First, let's create copies of the nested objects.
         some_list_of_objects = copy.copy(self.some_list_of_objects)
         some_circular_ref = copy.copy(self.some_circular_ref)
 
-        new = self.__class__(self.some_int, some_list_of_objects, some_circular_ref)
+        # Then, let's clone the object itself, using the prepared clones of the nested objects.
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
         new.__dict__.update(self.__dict__)
 
         return new
 
-    # Creates a deep copy. This method will be called whenever someone calls 'copy.deepcopy' with this object and the returned value is new deep copy.
-    # Memo parameter is the dictionary that is used by the 'deepcopy' library to prevent infinite recursive copies in instances of circular references.
-    # Pass it to all the 'deepcopy' calls you make in the '__deepcopy__' implementation to prevent infinite recursions.
     def __deepcopy__(self, memo=None):
+        """
+        Create a deep copy. 
+        This method will be called whenever someone calls `copy.deepcopy` with this object and the returned value is returned as the new deep copy.
+
+        Memo is the dictionary that is used by the `deepcopy` library to prevent infinite recursive copies in instances of circular references.
+        Pass it to all the `deepcopy` calls you make in the `__deepcopy__` implementation to prevent infinite recursions.
+        """
         if memo is None:
             memo = {}
 
-        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo=memo)
-        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo=memo)
+        # First, let's create copies of the nested objects.
+        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
+        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
 
-        new = self.__class__(self.some_int, some_list_of_objects, some_circular_ref)
-        new.__dict__ = copy.deepcopy(self.__dict__, memo=memo)
+        # Then, let's clone the object itself, using the prepared clones of the nested objects.
+        new = self.__class__(
+            self.some_int, some_list_of_objects, some_circular_ref
+        )
+        new.__dict__ = copy.deepcopy(self.__dict__, memo)
 
         return new
 
