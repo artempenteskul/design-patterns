2046f9df5f520f4e0a90990dc3e622913c14c0f1
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-08-30 18:05:37 +0300

    refactoring

--- creational/abstract_factory/abstract_factory.py
+++ creational/abstract_factory/abstract_factory.py
@@ -85,6 +85,7 @@ class AbstractProductB(ABC):
 
 """ Concrete Products are created by corresponding Concrete Factories. """
 
+
 class ConcreteProductB1(AbstractProductB):
     def useful_function_b(self) -> str:
         return "The result of the product B1."
@@ -94,7 +95,7 @@ class ConcreteProductB1(AbstractProductB):
     Product A1. Nevertheless, it accepts any instance of AbstractProductA as an
     argument.
     """
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
         result = collaborator.useful_function_a()
         return f"The result of the B1 collaborating with the ({result})"
     
@@ -103,7 +104,7 @@ class ConcreteProductB2(AbstractProductB):
     def useful_function_b(self) -> str:
         return "The result of the product B2."
 
-    def another_useful_function_b(self, collaborator: AbstractProductA):
+    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
         result = collaborator.useful_function_a()
         return f"The result of the B2 collaborating with the ({result})"
     
