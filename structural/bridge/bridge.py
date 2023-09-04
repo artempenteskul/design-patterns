103eee50993051c630aa38544525b360bab8e8dd
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-09-04 21:09:13 +0300

    refactoring

--- structural/bridge/bridge.py
+++ structural/bridge/bridge.py
@@ -36,6 +36,7 @@ class Implementation(ABC):
 Each Concrete Implementation corresponds to a specific platform and implements the Implementation interface using that platform's API.
 """
 
+
 class ConcreteImplementationA(Implementation):
     def operation_implementation(self) -> str:
         return "ConcreteImplementationA: Here's the result on the platform A."
